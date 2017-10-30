df335fa0774afd64e868c118691a607dbe898a32
Shrutina Agarwal <shrutina.agarwal10@gmail.com>
2017-10-30 11:37:46 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+from os import listdir
+from PIL import Image as PImage
 def vectorMag(S,S_target,n):
 	sum=0,sqSum=0
 	for i in range(n):
@@ -15,7 +18,7 @@ def Calc_sigma(S,S_target):
 def Calc_weight(S, S_target):
 	sqSum=0
 	sigma = Calc_sigma(S,S_target);
-	for i in range(n):
+	for i in range(N):
 		sqSum = 0
 		for j in range(36):
 			sqSum+= (S[i][j]-S_target[j])^2
@@ -24,3 +27,33 @@ def Calc_weight(S, S_target):
  
 alpha=0.9
 w=[0]* N
+
+def loadImages(path):
+    # return array of images
+
+    imagesList = listdir(path)
+    loadedImages = []
+    for image in imagesList:
+        img = PImage.open(path + image)
+        loadedImages.append(img)
+
+    return loadedImages
+
+def Calc_Median(I_k)
+	path = "/path/to/images"     # the variable contains path to the image dataset
+
+	
+	I_i = loadImages(path)
+	min_val=0;
+
+	for i in range(N):
+		pix1 = numpy.array(I_i[i]);
+		pix2 = numpy.array(I_k);
+		x= np.absolute(pix1-pix2)*w[i]
+		if(i==1||min_val>x)
+			min_val = x
+			median_pix = pix1
+			median_img = I_i[i]
+			
+			
+		
